8960c10cdd4cb041bd151607ae9a63c6bbf5fd96
Sergio Berdiell <sergiovencillon10@gmail.com>
2021-01-25 14:30:07 +0100

    Primera copia

---
+++ javaScript/JS.js
@@ -0,0 +1,11 @@
+
+/* Interaccion del menu como desplegable RESPONSIVE DESIGN */
+
+const boton = document.getElementById('button');
+const menu = document.getElementById('menu');
+
+boton.addEventListener('click', ()=>{
+menu.classList.toggle('mostrar')
+})
+
+
